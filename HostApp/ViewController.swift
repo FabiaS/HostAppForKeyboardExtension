db733252fe861c593e0742a51e8d16de9a399b6b
Fabia Schaeufele <fabia@novoda.com>
2017-02-16 18:35:17 +0100

    removes surplus code in the host app

--- HostApp/ViewController.swift
+++ HostApp/ViewController.swift
@@ -2,13 +2,5 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
 }
 
